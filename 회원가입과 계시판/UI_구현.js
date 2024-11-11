da0b2634c3b88b1b723a1f2d0c8e5c0e962356c9
smdh1458 <smdh1458@hanmail.net>
2024-11-11 18:17:59 +0900

    방과후 보충

---
+++ 회원가입과 계시판/UI_구현.js
@@ -0,0 +1,14 @@
+function showmethehint() {
+  const hint1 = document.getElementById("hint1");
+  hint1.style.display = "block";
+}
+function showhint() {
+  const hint = document.getElementById("hint");
+  hint.style.color = "red";
+  hint.style.display = "block";
+}
+
+function nonehint() {
+  const hint = document.getElementById("hint");
+  hint.style.display = "none";
+}
